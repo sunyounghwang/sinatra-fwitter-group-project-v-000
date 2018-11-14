0ce5d52ca20676ec02b41ae807543e40851680c2
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-11-14 21:08:46 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,20 +11,28 @@ class UsersController < ApplicationController
       session[:user_id] = user.id
       redirect to "/tweets"
     end
-    redirect to "/signup" #redirects if failed user creation
+    redirect to "/signup" #redirects if user creation fails
   end
 
   get "/login" do
-    redirect to "/tweets" if logged_in?
+    redirect to "/tweets" if logged_in? #redirects if user logged_in
     erb :"users/login"
   end
 
   post "/login" do
     user = User.find_by(username: params[:username])
-    if user && user.authenticate(params[:password])
+    if user && user.authenticate(params[:password]) #validates both username & password are found
       session[:user_id] = user.id
       redirect to "/tweets"
     end
+  end
+
+  get "/logout" do
+    if logged_in?
+      session.clear
+      redirect to "/login"
+    end
+    redirect to "/"
   end
 
 end
